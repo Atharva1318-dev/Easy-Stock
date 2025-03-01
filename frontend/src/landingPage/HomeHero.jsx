022a193a88d4a57cc4a6d6330fc81bf96bc2cd8a
Atharva <atharvajofficial@email.com>
2025-03-01 20:27:57 +0530

    styling

--- frontend/src/landingPage/HomeHero.jsx
+++ frontend/src/landingPage/HomeHero.jsx
@@ -25,10 +25,10 @@ export default function HomeHero({ username }) {
 
     return (
         <div className="HomeHero container-fluid px-0 mt-2 mt-md-1 mb-5">
-            <div className="row g-3 align-items-center">
+            <div className="row g-0 align-items-center">
                 {/* Text Content Column */}
                 <div className="col-12 col-lg-4 order-2 order-lg-1">
-                    <div className="px-1 px-md-1 py-4 py-md-5">
+                    <div className="px-1 px-md-0 py-4 py-md-5">
                         <h1 className="focus-in-expand display-5 fw-bold text-start mb-4 mainHeading">
                             <b>
                                 Seamless&nbsp;Trading <br />
