0dbf0373fb8424504129e3e251c8bdde28799098
Atharva <atharvajofficial@email.com>
2025-03-07 12:54:37 +0530

    styling

--- frontend/src/landingPage/EcosystemCard.jsx
+++ frontend/src/landingPage/EcosystemCard.jsx
@@ -1,6 +1,6 @@
 function EcosystemCard({ title, text }) {
     return (
-        <div className="card mb-3 mx-2 border-1 shadow rounded-4" style={{ width: "550px", height: "190px" }}>
+        <div className="card mb-3 mx-2 border-1 shadow rounded-4" style={{ width: "550px", height: "170px" }}>
             <div className="card-body">
                 <h5 className="card-title" style={{ color: "#7fb6ef" }}>{title}</h5>
                 <hr className="m-0 pb-2" />
